b7ae5789fd41fdc6bab6f27c7ba643c58a0a806b
AlisaWalker2004 <116071232+AlisaWalker2004@users.noreply.github.com>
2024-03-11 22:29:53 +0400

    First

---
+++ HelpDesk/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using HelpDesk.MyFrame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelpDesk
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly AndreevaHelpDeskEntities entities;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbLogin.Text != String.Empty && tbPassword.Text != String.Empty)
+            {
+                var user = AndreevaHelpDeskEntities.GetContext().Employees.Where(u => u.Login == tbLogin.Text
+                && u.Password == tbPassword.Text).FirstOrDefault();
+                spAutho.Visibility = Visibility.Hidden;
+
+
+                if (user == null)
+                {
+                    MessageBox.Show("Неправильно введён логин или пароль!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if (user.Login == "AA1")
+                {
+                    MessageBox.Show("Вы авторизовались!", "Приветствие", MessageBoxButton.OK, MessageBoxImage.Information);
+                    (App.Current as App).idUser = user.idSotr.ToString();
+                    MainFrame.NavigationService.Navigate(new Uri("PageAdmin.xaml", UriKind.Relative));
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Вы авторизовались!", "Приветствие", MessageBoxButton.OK, MessageBoxImage.Information);
+                    
+                    MainFrame.Navigate(new PageUser());
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ HelpDesk/MyFrame/PageUser.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelpDesk.MyFrame
+{
+    /// <summary>
+    /// Логика взаимодействия для PageUser.xaml
+    /// </summary>
+    public partial class PageUser : Page
+    {
+        AndreevaHelpDeskEntities entities;
+        public PageUser()
+        {
+            InitializeComponent();
+            cbTypeOfIncident.ItemsSource = AndreevaHelpDeskEntities.GetContext().TypeOfIncidents.ToList();
+        }
+
+        private void btnAddIncident_Click(object sender, RoutedEventArgs e)
+        {
+            var idTypeOfIncident = entities.TypeOfIncidents.Where(x=>x.TypeOfIncident1 == cbTypeOfIncident.Text);
+            //var a = new List<Incident>()
+            //{
+            //    new Incident() { Description = ComboVaccinations.Text + " "+ txtProblem.Text, id_Docors = idDoctorRandom.Next(1, kolDoctor), id_TypeOfService = idTypeOfService,
+            //    id_PassportOfTheAnimal = idAnimalCombo, Date = dpDateZapis.SelectedDate.Value}
+            // };
+        }
+    }
+}
